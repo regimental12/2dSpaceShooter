f4a4a6fb927c91cb3ad5976d1faffd622faf3729
regimental12 <richard.hankins@gmail.com>
2014-11-22 22:55:01 +0000

    enemy shooting

---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour {
+
+    public float coolDown = 0f;
+    public float fireDelay = 0.50f;
+    public GameObject bulletPrefab;
+    public Vector3 bulletOffset = new Vector3(0, 0.75f, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        coolDown -= Time.deltaTime;
+        if ( coolDown <= 0)
+        {
+            //Shoot
+            Debug.Log("pew!");
+            coolDown = fireDelay;
+
+            Vector3 offSet = transform.rotation * bulletOffset;
+
+            GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, transform.position + offSet, transform.rotation);
+
+            bulletGo.layer = gameObject.layer;
+        }
+        if (coolDown <= 0)
+        {
+            coolDown = 0;
+        }
+    }
+}
